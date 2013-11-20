560e3b06f8957b76c6df797e108e161319caea67
koeZionCMS <github@koezion-cms.com>
2013-11-20 14:09:17 +0100

    Version du 20/11/2013 - 20131120.001
    Cf versions.xml

--- webroot/js/ckeditor/config.js
+++ webroot/js/ckeditor/config.js
@@ -29,7 +29,7 @@ For licensing, see LICENSE.html or http://ckeditor.com/license
 	    { name: 'basicstyles', items : [ 'Bold','Italic','Underline','Strike','Subscript','Superscript','-','RemoveFormat' ] },
 	    { name: 'paragraph',   items : [ 'NumberedList','BulletedList','-','Outdent','Indent','-','Blockquote','-','JustifyLeft','JustifyCenter','JustifyRight','JustifyBlock','-','BidiLtr','BidiRtl' ] },
 	    { name: 'links',       items : [ 'Link','Unlink','Anchor' ] },
-	    { name: 'insert',      items : [ 'Image','Flash','Table','HorizontalRule','SpecialChar','Code' ] },
+	    { name: 'insert',      items : [ 'Image','Flash','Iframe','Table','HorizontalRule','SpecialChar','Code' ] },
 	    '/',
 	    { name: 'styles',      items : [ 'Styles','Format','Font','FontSize' ] },
 	    { name: 'colors',      items : [ 'TextColor','BGColor' ] },
