e5464d6080c9b25fdebc83957832ff298a114619
koeZionCMS <github@koezion-cms.com>
2014-03-18 10:37:20 +0100

    Version du 18/03/2014 - 20140318.001
    Cf versions.xml

--- core/Koezion/object.php
+++ core/Koezion/object.php
@@ -19,7 +19,54 @@ class Object {
  *
  * @param varchar $name Nom du model à charger
  * @version 0.1 - 23/12/2011
+ * @version 0.2 - 18/03/2014 - Reprise du chargement des modèles des plugins
  */
+	function loadModel($name, $return = false) {
+		
+		//En premier lieu on test si le model n'est pas déjà instancié
+		//et si il ne l'est pas on procède à son intenciation
+		if(!isset($this->$name)) {
+			
+			$file_path = '';				
+			$file_name = Inflector::underscore($name).'.php'; //Nom du fichier à charger		
+			$file_path_default = ROOT.DS.'models'.DS.$file_name; //Chemin vers le fichier à charger
+			
+			//Pour déterminer le dossier du plugin nous devons transformer le nom du model à charger
+			//Etape 1 : passage au pluriel
+			//Etape 2 : transformation du camelCased en _
+			$pluginPluralizeName = Inflector::pluralize($name);		
+			$pluginUnderscoreName = Inflector::underscore($pluginPluralizeName);
+			
+			//////////////////////////////////////////////
+			//   RECUPERATION DES CONNECTEURS PLUGINS   //
+			$pluginsConnectors = get_plugins_connectors();
+			if(isset($pluginsConnectors[$pluginUnderscoreName])) {
+					
+				$connectorModel = $pluginsConnectors[$pluginUnderscoreName];
+				$file_path_plugin = PLUGINS.DS.$connectorModel.DS.'models'.DS.$file_name;	
+			}
+			//////////////////////////////////////////////		
+			
+			if(isset($file_path_plugin) && file_exists($file_path_plugin)) { $file_path = $file_path_plugin; } //Si il y a un plugin on le charge par défaut		
+			else if(file_exists($file_path_default)) { $file_path = $file_path_default; } //Sinon on test si le model par défaut existe
+			
+			if(!file_exists($file_path)) { 				
+				
+				Session::write('redirectMessage', "Impossible de charger le modèle ".$name." dans le fichier controller");
+				$this->redirect('home/e404');
+				die();
+			} //On va tester l'existence de ce fichier
+			
+			require_once($file_path); //Inclusion du fichier
+			
+			if(isset($this->request->fullUrl)) { $url = $this->request->fullUrl; } else { $url = null; }			
+			if($return) { return new $name($url); }
+			else { $this->$name = new $name($url); } //Création d'un objet Model de type $name que l'on va instancier dans la classe
+		}
+	}
+	
+	/*
+	ANCIENNE VERSION AU CAS OU
 	function loadModel($name, $return = false) {
 		
 		//En premier lieu on test si le model n'est pas déjà instancié
@@ -64,7 +111,7 @@ class Object {
 			if($return) { return new $name($url); }
 			else { $this->$name = new $name($url); } //Création d'un objet Model de type $name que l'on va instancier dans la classe
 		}
-	}
+	}*/
 	
 	/**
 	 * Cette fonction permet le "déchargement" d'un model dans un controller
