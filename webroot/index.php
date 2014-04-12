7d852d4b17bbf7baae62dc5ce41fab547b65db96
koeZionCMS <github@koezion-cms.com>
2014-04-12 12:53:25 +0200

    Version du 12/04/2014 - 20140412.004
    Cf versions.xml

--- webroot/index.php
+++ webroot/index.php
@@ -17,77 +17,7 @@
  * @link        http://www.koezion-cms.com
  * @version		0.1
  */
-//$debut = microtime(true);
-define('DS', DIRECTORY_SEPARATOR); //Définition du séparateur dans le cas ou l'on est sur windows ou linux
-
-////////////////////////////////////////////////////////////////////////////////////
-//   MISE EN PLACE DES CONSTANTES VERS LES DIFFERENTS DOSSIERS (CHEMINS ASSOLUS)  //
-define('WEBROOT', dirname(__FILE__)); //Chemin vers le dossier webroot
-	define('CSS', WEBROOT.DS.'css'); //Chemin vers le dossier css
-	define('FILES', WEBROOT.DS.'files'); //Chemin vers le dossier files
-	define('IMG', WEBROOT.DS.'img'); //Chemin vers le dossier img
-	define('JS', WEBROOT.DS.'js'); //Chemin vers le dossier js
-	define('WEBROOT_PLUGINS', WEBROOT.DS.'plugins'); //Chemin vers le dossier plugins 
-	define('UPLOAD', WEBROOT.DS.'upload'); //Chemin vers le dossier upload
-
-define('ROOT', dirname(WEBROOT)); //Chemin vers le dossier racine du site
-
-define('CONFIGS', ROOT.DS.'configs'); //Chemin vers le dossier config
-	define('CONFIGS_FILES', CONFIGS.DS.'files'); //Chemin vers le dossier des .ini
-	define('CONFIGS_VERSIONS', CONFIGS.DS.'versions'); //Chemin vers le dossier des fichiers de versions
-	define('CONFIGS_HOOKS', CONFIGS.DS.'hooks'); //Chemin vers le dossier des hooks
-	define('CONFIGS_LAYOUT', CONFIGS.DS.'layout'); //Chemin vers le dossier des layout
-	define('CONFIGS_PLUGINS', CONFIGS.DS.'plugins'); //Chemin vers le dossier des plugins
-
-define('CONTROLLERS', ROOT.DS.'controllers'); //Chemin vers le dossier des contrôleurs
-	define('COMPONENTS', CONTROLLERS.DS.'components'); //Chemin vers le dossier des composants
-
-define('CORE', ROOT.DS.'core'); //Chemin vers le coeur de l'application
-	define('CAKEPHP', CORE.DS.'CakePhp'); //Chemin vers les librairies de cakePhp
-	define('GETTEXT', CORE.DS.'GetText'); //Chemin vers les librairies de getText
-	define('KOEZION', CORE.DS.'Koezion'); //Chemin vers les librairies de Koezion
-	define('LIBS', CORE.DS.'Libs'); //Chemin vers les librairies diverses
-	define('PEAR', CORE.DS.'Pear'); //Chemin vers les librairies de Pear
-	define('SWIFTMAILER', CORE.DS.'SwiftMailer'); //Chemin vers les librairies de SwiftMailer
-
-define('INSTALL_FILES', ROOT.DS.'install'.DS.'files'); //Chemin vers le dossier des traductions
-//define('LOCALE', ROOT.DS.'locale'); //Chemin vers le dossier des traductions
-
-define('MODELS', ROOT.DS.'models'); //Chemin vers le dossier des modèles
-	define('BEHAVIORS', MODELS.DS.'behaviors'); //Chemin vers le dossier des comportements
-	
-define('PLUGINS', ROOT.DS.'plugins'); //Chemin vers le dossier des plugins	
-	
-define('TMP', ROOT.DS.'tmp'); //Chemin vers le dossier temporaire
-	define('CACHE', TMP.DS.'cache'); //Chemin vers le dossier cache
-	define('LOGS', TMP.DS.'logs'); //Chemin vers le dossier logs
-
-define('VIEWS', ROOT.DS.'views'); //Chemin vers le dossier des vues
-	define('ELEMENTS', VIEWS.DS.'elements'); //Chemin vers le dossier des élements
-	define('HELPERS', VIEWS.DS.'helpers'); //Chemin vers le dossier des helpers
-	define('LAYOUT', VIEWS.DS.'layout'); //Chemin vers le dossier des layouts
-////////////////////////////////////////////////////////////////////////////////////	
-	
-/////////////////////////////////////////////////////////
-//   MISE EN PLACE DU CHEMIN RELATIF VERS LE WEBROOT   //
-//http://www.siteduzero.com/forum-83-692076-p1-base-url.html
-//define('BASE_URL', dirname(dirname($_SERVER['SCRIPT_NAME']))); //Chemin relatif vers le coeur de l'application --> OLD
-//Ne marche plus si le site est dans plus d'un sous dossier
-//24/07/2012 --> Correctif : maintenant tout fonctionne même si plus d'un sous dossier, correction de la génération de la variable $baseUrl
-$baseUrl = '';
-$scriptPath = preg_split("#[\\\\/]#", dirname(__FILE__), -1, PREG_SPLIT_NO_EMPTY);
-$urlPath = preg_split("#[\\\\/]#", $_SERVER['REQUEST_URI'], -1, PREG_SPLIT_NO_EMPTY);
-
-foreach($urlPath as $k => $v) {
-	
-	$key = array_search($v, $scriptPath);
-	if($key !== false) $baseUrl .= "/".$v;
-}
-define('BASE_URL', $baseUrl); //Chemin relatif vers le coeur de l'application
-/////////////////////////////////////////////////////////
-
-define('GENERATOR_META', 'koéZion CMS - CMS OPENSOURCE PHP');
-define('GENERATOR_LINK', '<p id="powered_by" style="position:absolute;width:20px;bottom:5px;right:5px;font-size:8px;margin-bottom:0;height:20px;opacity:.3"><a href="http://www.koezion-cms.com" title="Propulsé par koéZionCMS - CMS opensource PHP" style="width:20px;height:20px;text-indent:-9999px;display:block;background: url('.BASE_URL.'/img/logo_koezion_mini.png) no-repeat top right transparent;">Propulsé par koéZionCMS - CMS opensource PHP</a></p>');
+require_once('constants.php');
 
 require_once KOEZION.DS.'bootstrap.php'; //Premier fichier lancé par l'application
 
