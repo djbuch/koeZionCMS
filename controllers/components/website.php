0512b9ab7f1bf795b6f7b95f80077b339e9f7b39
koeZionCMS <github@koezion-cms.com>
2014-05-02 10:39:01 +0200

    Version du 02/05/2014 - 20140502.001
    Cf versions.xml

--- controllers/components/website.php
+++ controllers/components/website.php
@@ -24,10 +24,12 @@ class WebsiteComponent extends Component {
  * @version 0.1 - 02/05/2012 by FI
  * @version 0.2 - 14/06/2012 by FI - Modification de la récupération du site pour la boucle locale - On récupère le premier site de la liste et plus celui avec l'id 1 pour éviter les éventuelles erreurs
  * @version 0.3 - 04/09/2012 by FI - Mise en place d'un passage de paramètre en GET pour pouvoir changer de site en local
+ * @version 0.4 - 02/04/2014 by FI - Mise en place d'un passage de paramètre en GET pour pouvoir changer le host du site en local
  */
 	public function get_website_datas() {
 	
 		$httpHost = $_SERVER["HTTP_HOST"]; //Récupération de l'url
+		if(isset($_GET['hack_ws_host'])) { $httpHost = $_GET['hack_ws_host']; }
 	
 		$cacheFolder 	= TMP.DS.'cache'.DS.'variables'.DS.'Websites'.DS;
 		$cacheFile 		= $httpHost;
@@ -39,8 +41,8 @@ class WebsiteComponent extends Component {
 			$this->loadModel('Website'); //Chargement du modèle
 	
 			//HACK SPECIAL LOCAL POUR CHANGER DE SITE pour permettre la passage de l'identifiant du site en paramètre
-			if(isset($_GET['hack_current_website_id'])) { Session::write('Frontoffice.hack_current_website_id', $_GET['hack_current_website_id']); }
-			$hackCurrentWebsiteId = Session::read('Frontoffice.hack_current_website_id');
+			if(isset($_GET['hack_ws_id'])) { Session::write('Frontoffice.hack_ws_id', $_GET['hack_ws_id']); }
+			$hackCurrentWebsiteId = Session::read('Frontoffice.hack_ws_id');
 	
 			if($httpHost == 'localhost' || $httpHost == '127.0.0.1') {
 	
