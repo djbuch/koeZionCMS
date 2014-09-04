9a7747b7ff9b62fcad47a780f3d18e12de8bd18d
koeZionCMS <github@koezion-cms.com>
2014-09-04 16:20:28 +0200

    Version du 04/09/2014 - 20140904.001
    Cf versions.xml

--- webroot/templates/empty/views/layout/empty.php
+++ webroot/templates/empty/views/layout/empty.php
@@ -7,6 +7,7 @@
 		if(isset($description_for_layout) && !empty($description_for_layout)) { echo "\t\t"; echo '<meta name="description" content="'.$description_for_layout.'" />'; echo "\n"; }
 		if(isset($keywords_for_layout) && !empty($keywords_for_layout)) { echo "\t\t"; echo '<meta name="keywords" content="'.$keywords_for_layout.'" />'; echo "\n"; }
 		if(isset($rss_for_layout) && !empty($rss_for_layout)) { echo "\t\t"; echo '<link rel="alternate" type="application/rss+xml" title="'.$rss_for_layout['title'].'" href="'.$rss_for_layout['link'].'" />'; echo "\n"; }
+		if(isset($websiteParams['favicon']) && !empty($websiteParams['favicon'])) { echo "\t\t"; echo '<link rel="icon" type="image/png" href="'.$websiteParams['favicon'].'" />'; echo "\n"; }
 		?>
 		<meta name="generator" content="<?php echo GENERATOR_META; ?>" /><?php //ATTENTION VOUS NE POUVEZ PAS SUPPRIMER CETTE BALISE ?>		
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
