230f7ce9db6a5fd54ccbecba55acb089c5628795
koeZionCMS <github@koezion-cms.com>
2014-09-12 16:53:33 +0200

    Version du 12/09/2014 - 20140912.002
    Cf versions.xml

--- views/elements/backoffice/left.php
+++ views/elements/backoffice/left.php
@@ -36,111 +36,116 @@ $jsContextMenu = array(
 );
 */
 $jsContextMenu = array();
-?>
-<div id="left">
-	<span class="menu">Menu</span>
-	<ul>
-		<?php 
-		$cpt = 0;
-		foreach($leftMenus as $k => $v) {
-			
-			if(count($v['menus'])) {
-				?>
-				<li>
-					<?php 
-					if(!empty($v['libelle'])) { ?><a><?php echo $v['libelle']; ?></a><?php } 
-					$folderIdA = 'fold'.$cpt;
-					$cpt++;
-					$jsContextMenu[$folderIdA]['name'] = $v['libelle']; 
-					?>
-					<ul>
-						<?php 
-						foreach($v['menus'] as $key => $leftMenus) {
 
-							if(!is_int($key)) { 
-								?>
-								<li>
-									<?php 
-									if(!empty($key)) { ?><a class="under_section"><?php echo $key; ?></a><?php } 
-									$folderIdB = 'fold'.$cpt;
-									$cpt++;
-									$jsContextMenu[$folderIdA]['items'][$folderIdB]['name'] = $key;
-									?>
-									<ul>
-										<?php 
-										foreach($leftMenus as $leftMenu) {
-
-											$folderIdC = 'fold'.$cpt;
-											$cpt++;
-											
-											$action = !empty($leftMenu['action_name']) ? $leftMenu['action_name'] : 'index';
-											$link = '<a href="'.Router::url('backoffice/'.$leftMenu['controller_name'].'/'.$action).'">'.$leftMenu['name'].'</a>'; 
-											?><li><?php echo $link; ?></li><?php
-											$jsContextMenu[$folderIdA]['items'][$folderIdB]['items'][$folderIdC] = array('type' => 'html', 'html' => $link);
-										}
-										?>
-									</ul>
-								</li>
-								<?php 								
-							} else {
-
-								$folderIdD = 'fold'.$cpt;
-								$cpt++;
-							
-								$action = !empty($leftMenus['action_name']) ? $leftMenus['action_name'] : 'index';		
-								$link = '<a href="'.Router::url('backoffice/'.$leftMenus['controller_name'].'/'.$action).'">'.$leftMenus['name'].'</a>';				
-								?><li><?php echo $link; ?></li><?php								
-								$jsContextMenu[$folderIdA]['items'][$folderIdD] = array('type' => 'html', 'html' => $link);
-							} 
-						} 
-						?>
-					</ul>
-				</li>
-				<?php 
-			}
-		} 
+ob_start();
+$cpt = 0;
+foreach($leftMenus as $k => $v) {
+	
+	if(count($v['menus'])) {
 		?>
-	</ul>
+		<li>
+			<?php 
+			if(!empty($v['libelle'])) { ?><a><?php echo $v['libelle']; ?></a><?php } 
+			$folderIdA = 'fold'.$cpt;
+			$cpt++;
+			$jsContextMenu[$folderIdA]['name'] = $v['libelle']; 
+			?>
+			<ul>
+				<?php 
+				foreach($v['menus'] as $key => $leftMenus) {
+
+					if(!is_int($key)) { 
+						?>
+						<li>
+							<?php 
+							if(!empty($key)) { ?><a class="under_section"><?php echo $key; ?></a><?php } 
+							$folderIdB = 'fold'.$cpt;
+							$cpt++;
+							$jsContextMenu[$folderIdA]['items'][$folderIdB]['name'] = $key;
+							?>
+							<ul>
+								<?php 
+								foreach($leftMenus as $leftMenu) {
+
+									$folderIdC = 'fold'.$cpt;
+									$cpt++;
+									
+									$action = !empty($leftMenu['action_name']) ? $leftMenu['action_name'] : 'index';
+									$link = '<a href="'.Router::url('backoffice/'.$leftMenu['controller_name'].'/'.$action).'">'.$leftMenu['name'].'</a>'; 
+									?><li><?php echo $link; ?></li><?php
+									$jsContextMenu[$folderIdA]['items'][$folderIdB]['items'][$folderIdC] = array('type' => 'html', 'html' => $link);
+								}
+								?>
+							</ul>
+						</li>
+						<?php 								
+					} else {
+
+						$folderIdD = 'fold'.$cpt;
+						$cpt++;
+					
+						$action = !empty($leftMenus['action_name']) ? $leftMenus['action_name'] : 'index';		
+						$link = '<a href="'.Router::url('backoffice/'.$leftMenus['controller_name'].'/'.$action).'">'.$leftMenus['name'].'</a>';				
+						?><li><?php echo $link; ?></li><?php								
+						$jsContextMenu[$folderIdA]['items'][$folderIdD] = array('type' => 'html', 'html' => $link);
+					} 
+				} 
+				?>
+			</ul>
+		</li>
+		<?php 
+	}
+} 
+$contentMenu = ob_get_clean();
+if(!empty($contentMenu)) {
 	
-	<script type="text/javascript">	
-	$(document).ready(function() { 
-	   	   
-	   var screenSurface = $.get_surface_ecran();
-	   var mainHeight = screenSurface.windowHeight - 52;
-	   $('#wrapper #right #main').css('min-height', mainHeight);
-	   
-	    /**************************************************
-	     * Context-Menu with Sub-Menu
-	     **************************************************/
-	    $.contextMenu({
-	        selector: '#wrapper #right #main',
-	        items: <?php echo $helpers['Json']->encode($jsContextMenu); ?>
-	    });
-	});
-	</script>		
-	
-	<?php /* ?>
-	<div id="social">
-		<?php //PARTAGE FACEBOOK ?>
-		<div class="facebook">
-			<div id="fb-root"></div>
-			<script>
-			(function(d, s, id) {
-				var js, fjs = d.getElementsByTagName(s)[0];
-				if (d.getElementById(id)) return;
-				js = d.createElement(s); js.id = id;
-				js.src = "//connect.facebook.net/fr_FR/all.js#xfbml=1";
-				fjs.parentNode.insertBefore(js, fjs);
-			}(document, 'script', 'facebook-jssdk'));
-			</script>	
-			<div class="fb-like" data-href="https://www.facebook.com/koezioncms" data-layout="button_count" data-action="like" data-show-faces="true" data-share="false"></div>
+	?>			
+	<div id="left">
+		<span class="menu">Menu</span>
+		<ul>
+			<?php echo $contentMenu; ?>
+		</ul>
+		
+		<script type="text/javascript">	
+		$(document).ready(function() { 
+		   	   
+		   var screenSurface = $.get_surface_ecran();
+		   var mainHeight = screenSurface.windowHeight - 52;
+		   $('#wrapper #right #main').css('min-height', mainHeight);
+		   
+		    /**************************************************
+		     * Context-Menu with Sub-Menu
+		     **************************************************/
+		    $.contextMenu({
+		        selector: '#wrapper #right #main',
+		        items: <?php echo $helpers['Json']->encode($jsContextMenu); ?>
+		    });
+		});
+		</script>		
+		
+		<?php /* ?>
+		<div id="social">
+			<?php //PARTAGE FACEBOOK ?>
+			<div class="facebook">
+				<div id="fb-root"></div>
+				<script>
+				(function(d, s, id) {
+					var js, fjs = d.getElementsByTagName(s)[0];
+					if (d.getElementById(id)) return;
+					js = d.createElement(s); js.id = id;
+					js.src = "//connect.facebook.net/fr_FR/all.js#xfbml=1";
+					fjs.parentNode.insertBefore(js, fjs);
+				}(document, 'script', 'facebook-jssdk'));
+				</script>	
+				<div class="fb-like" data-href="https://www.facebook.com/koezioncms" data-layout="button_count" data-action="like" data-show-faces="true" data-share="false"></div>
+			</div>
+			<div class="twitter">
+				<?php //PARTAGE TWITTER ?>
+				<a href="https://twitter.com/share" class="twitter-share-button" data-url="https://twitter.com/KoeZionCMS" data-text="KoéZionCMS - Solution de Gestion de Contenu - CMS Open Source PHP" data-lang="fr" data-dnt="true">Tweeter</a>
+				<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
+			</div>
 		</div>
-		<div class="twitter">
-			<?php //PARTAGE TWITTER ?>
-			<a href="https://twitter.com/share" class="twitter-share-button" data-url="https://twitter.com/KoeZionCMS" data-text="KoéZionCMS - Solution de Gestion de Contenu - CMS Open Source PHP" data-lang="fr" data-dnt="true">Tweeter</a>
-			<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
-		</div>
+		<?php */ ?>
+		<div id="credits">&#169; Copyright 2010 - <?php echo date('Y'); ?> koéZionCMS</div>
 	</div>
-	<?php */ ?>
-	<div id="credits">&#169; Copyright 2010 - <?php echo date('Y'); ?> koéZionCMS</div>
-</div>
+<?php } ?>
