e36eb819166c86a0661296600a6accca76b8e8a6
koeZionCMS <github@koezion-cms.com>
2014-10-14 17:19:38 +0200

    Version du 14/10/2014 - 20141014.001
    Cf versions.xml

--- views/elements/backoffice/listing/categories/search.php
+++ views/elements/backoffice/listing/categories/search.php
@@ -1,13 +1,9 @@
 <div style="border:1px solid #E5E5E5;margin-bottom:30px;" class="intern_search">
 	<div class="title">
 		<h2>Moteur de recherche</h2>
-		<?php 
-		$defaultCss = '';
-		if(!isset($this->controller->request->data['Search'])) { $defaultCss = "default"; } 
-		?>
-		<div class="hide <?php echo $defaultCss; ?>" style="opacity: 1;"></div>
-	</div>	
-	<div class="content nopadding">	
+		<div class="hide" style="opacity: 1;"></div>
+	</div>
+	<div class="content nopadding" <?php if(isset($this->controller->request->data['Search'])) { ?>style="display:block;"<?php } ?>>		
 		<?php
 		$formOptions = array('action' => Router::url('backoffice/'.$params['controllerFileName']), 'method' => 'get', 'id' => 'formSearch');
 		echo $helpers['Form']->create($formOptions);
