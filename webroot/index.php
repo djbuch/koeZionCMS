4096c1209183e4067ef8eeb6f3b5084c336b54cd
AnisJomni <jomni.anis@mail.com>
2015-02-11 17:48:08 +0100

    Version du 11/02/2015 - 20150211.001
    Cf versions.xml

--- webroot/index.php
+++ webroot/index.php
@@ -17,14 +17,13 @@
  * @link        http://www.koezion-cms.com
  * @version		0.1
  */
- 
- 
 require_once('constants.php');
  
 //01/08/2012 - Rajout d'un test pour savoir si le site est correctement paramétré
 //Si le fichier database n'existe pas cela veut dire que le site n'est pas correctement paramétré
 //il faut donc rediriger vers le dossier d'installation
 if(!file_exists(CONFIGS.DS.'files'.DS.'database.ini')) {
+	
 	require_once KOEZION.DS.'router.php'; //Chargement de l'object Router (Analyse des Urls)
 	header("Location:".Router::url('/install',''));
 	die();
