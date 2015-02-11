e31d33a396c0baf45ace2c5d77d11319b8d400d8
AnisJomni <jomni.anis@mail.com>
2015-02-11 17:37:29 +0100

    Version du 11/02/2015 - 20150211.001
    Problème au niveau du Header, l'objet Router n'est pas chargé ce qui
    provoque une mauvaise direction et donc une installation non
    automatique. Rajout ligne 28 dans le fichier
    koeZionCMS/webroot/index.php

--- webroot/index.php
+++ webroot/index.php
@@ -17,18 +17,20 @@
  * @link        http://www.koezion-cms.com
  * @version		0.1
  */
+ 
+ 
 require_once('constants.php');
-
-require_once KOEZION.DS.'bootstrap.php'; //Premier fichier lancé par l'application
-
+ 
 //01/08/2012 - Rajout d'un test pour savoir si le site est correctement paramétré
 //Si le fichier database n'existe pas cela veut dire que le site n'est pas correctement paramétré
 //il faut donc rediriger vers le dossier d'installation
 if(!file_exists(CONFIGS.DS.'files'.DS.'database.ini')) {
-
-	header("Location: ".Router::url('/install', ''));
+	require_once KOEZION.DS.'router.php'; //Chargement de l'object Router (Analyse des Urls)
+	header("Location:".Router::url('/install',''));
 	die();
 }
+
+require_once KOEZION.DS.'bootstrap.php'; //Premier fichier lancé par l'application
 
 new Dispatcher(); //On créé une instance de Dispatcher
 //if(Configure::read('debug') > 0) { Configure::write('timerExec', round(microtime(true) - $debut, 5)); }
