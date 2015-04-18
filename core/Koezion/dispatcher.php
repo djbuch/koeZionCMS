8ab120e3a03ad9be636871ac5620adc3b7f0825a
koeZionCMS <github@koezion-cms.com>
2015-04-18 10:08:27 +0200

    Version du 18/04/2015 - 20150418.001
    Cf versions.xml

--- core/Koezion/dispatcher.php
+++ core/Koezion/dispatcher.php
@@ -4,6 +4,7 @@
  * - Instancier un objet de type Request qui va récupérer l'url
  * - Parser cette url via l'objet Router
  * - Charger le controller souhaité
+ * - Lancer l'action demandée
  * 
  * PHP versions 4 and 5
  *
@@ -17,7 +18,15 @@
  * @link        http://www.koezion-cms.com
  */
 class Dispatcher {
-    
+
+/**
+ * Objet de type Request
+ * 
+ * @var object
+ * @access 	public
+ * @author 	koéZionCMS
+ * @version 0.1 - 30/01/2012 by FI  
+ */	
 	var $request;
     
 /**
@@ -38,16 +47,23 @@ class Dispatcher {
 	} 
 	
 /**
- * Fonction chargée de faire les appels des bons fichiers
+ * Fonction chargée d'initialiser le contrôleur et de lancer la fonction demandée
+ *
+ * @access	public
+ * @author	koéZionCMS
+ * @version 0.1 - 30/01/2012 by FI 
+ * @version 0.2 - 18/04/2015 by FI - Mise en place du test sur le premier caractère de la fonction pour savoir si elle est privée ou non
  */	
 	public function dispatch() {
 		
 		Router::parse($this->request->url, $this->request); //Parsing de l'url
-		$controller = $this->loadController(); //Chargement du controller
+		$controller = $this->load_controller(); //Chargement du controller
 		
 		//On va tester si il y a un prefixe
 		$action = $this->request->action;
 		if($this->request->prefix) { $action = $this->request->prefix.'_'.$action; }
+		
+		$isPrivateFunction = ($action[0] == '_') ? true : false; //On vérifie si la fonction demandée n'est pas une fonction privée
 		
 		//Gestion des erreurs pour savoir si l'action demandée existe
 		//On va supprimer les fonctions de la classe parente avec le array_diff
@@ -58,26 +74,32 @@ class Dispatcher {
 					get_class_methods($controller),
 					get_class_methods('Controller')
 				)
-			)
+			) 
+			|| $isPrivateFunction
 		) {
 			
 			$this->error('Le contrôleur '.$this->request->controller." n'a pas de méthode ".$action." dans le fichier dispatcher");			
 			die();
 		}
 		
-		//fonction qui permet d'appeler une fonction
-		//Cette fonction va permettre l'appel dynamique de la fonction (située dans le controller) demandée dans l'url
-				
-		$this->dispatchMethod($controller, $action, $this->request->params);		
-		/*call_user_func_array(
-			array($controller, $action),
-			$this->request->params
-		);*/
+		//Cette fonction va permettre l'appel dynamique de la fonction (située dans le controller) demandée dans l'url				
+		$this->dispatch_method($controller, $action, $this->request->params);
 		
 		if($controller->auto_render) { $controller->render($action); } //AUTO RENDER
 	}	
 	
-	public function dispatchMethod($controller, $action, $params = array()) {
+/**
+ * Cette fonction est chargée de faire appel à la fonction demandée dans le contrôleur voulu
+ * Cette appel se fait de façon dynamique en fonction du nombre de paramètres passés
+ * 
+ * @param object 	$controller Objet de type Controller
+ * @param varchar 	$action 	Action demandée
+ * @param array 	$params 	Tableau de paramètres
+ * @access	public
+ * @author	koéZionCMS
+ * @version 0.1 - 30/01/2012 by FI 
+ */	
+	public function dispatch_method($controller, $action, $params = array()) {
 				
 		switch (count($params)) {
 			case 0: 	$controller->{$action}(); 																break;
@@ -88,13 +110,29 @@ class Dispatcher {
 			case 5: 	$controller->{$action}($params[0], $params[1], $params[2], $params[3], $params[4]); 	break;
 			default: 	call_user_func_array(array($controller, $action), $params); 							break;
 		}
-	}		
+	}	
+	
+/**
+ * Cette fonction va charger un controller
+ * 
+ * @return 	object Objet correspondant au controller souhaité
+ * @access	public
+ * @author	koéZionCMS
+ * @version 0.1 - 30/01/2012 by FI 
+ */
+	public function load_controller() {
+	
+		$fileName 			= $this->load_controller_file();
+		$controllerName 	= Inflector::camelize($fileName); //On transforme le nom du fichier pour récupérer le nom du controller	
+		$controller 		=  new $controllerName($this->request); //Création d'une instance du controller souhaité dans lequel on injecte la request		
+		return $controller;
+	}	
 	
 /**
  * Cette fonction va charger le fichier contenant la classe d'un controller
  *
  * @param 	varchar	$controllerToLoad Contrôleur à charger
- * @return 	$name 	Objet correspondant au type de controller souhaité
+ * @return 	varchar Nom du contrôleur souhaité
  * @access	public
  * @author	koéZionCMS
  * @version 0.1 - 23/12/2011
@@ -103,12 +141,12 @@ class Dispatcher {
  * @version 0.4 - 12/04/2014 by FI - Suppression de _plugin dans le nom d'un controller de plugin 
  * @version 0.5 - 12/04/2014 by FI - Annulation suppression de _plugin dans le nom d'un controller de plugin car un plugin peut potentiellement avoir le même nom qu'un controller existant 
  */
-	public function loadControllerFile($controllerToLoad = null) {
+	public function load_controller_file($controllerToLoad = null) {
 		
 		if(isset($controllerToLoad) && !empty($controllerToLoad)) { $this->request->controller = $controllerToLoad; }
 		
-		$controller_name = strtolower($this->request->controller.'_controller'); //On récupère dans une variable le nom du controller	
-		$controller_path = CONTROLLERS.DS.$controller_name.'.php'; //On récupère dans une variable le chemin du controller
+		$controllerName = strtolower($this->request->controller.'_controller'); //On récupère dans une variable le nom du controller	
+		$controllerPath = CONTROLLERS.DS.$controllerName.'.php'; //On récupère dans une variable le chemin du controller
 		
 		//////////////////////////////////////////////
 		//   RECUPERATION DES CONNECTEURS PLUGINS   //
@@ -116,19 +154,19 @@ class Dispatcher {
 		$pluginsConnectors = get_plugins_connectors();
 		if(isset($pluginsConnectors[$this->request->controller])) {
 			
-			$this->request->pluginFolder = $sFolderPlugin = $pluginsConnectors[$this->request->controller]; //Récupération du dossier du plugin si le controller appellé est dans un connector d'un plugin
-			$controller_path = PLUGINS.DS.$sFolderPlugin.DS.'controllers'.DS.$controller_name.'.php';
-			$controller_name = strtolower($this->request->controller.'_plugin_controller');
+			$this->request->pluginFolder = $folderPlugin = $pluginsConnectors[$this->request->controller]; //Récupération du dossier du plugin si le controller appellé est dans un connector d'un plugin
+			$controllerPath = PLUGINS.DS.$folderPlugin.DS.'controllers'.DS.$controllerName.'.php';
+			$controllerName = strtolower($this->request->controller.'_plugin_controller');
 		}
 		//////////////////////////////////////////////
 	
-		if(file_exists($controller_path)) { 
+		if(file_exists($controllerPath)) { 
 			
-			if(isset($sFolderPlugin)) {
+			if(isset($folderPlugin)) {
 
 				//On doit contrôler si le plugin est installé en allant lire le fichiers
 				$pluginsList = Cache::exists_cache_file(TMP.DS.'cache'.DS.'variables'.DS.'Plugins'.DS, "plugins");
-				$pluginControllerToLoad = Inflector::camelize($sFolderPlugin);
+				$pluginControllerToLoad = Inflector::camelize($folderPlugin);
 				if(!isset($pluginsList[$pluginControllerToLoad])) {
 				
 					$message = "Le controller du plugin ".$this->request->controller." n'existe pas"." dans le fichier dispatcher ou n'est pas correctement installé";
@@ -136,34 +174,30 @@ class Dispatcher {
 					die();
 				}
 				
-				$pluginControllerBoostrap = PLUGINS.DS.$sFolderPlugin.DS.'controller.php';
+				$pluginControllerBoostrap = PLUGINS.DS.$folderPlugin.DS.'controller.php';
 				if(file_exists($pluginControllerBoostrap)) { require_once($pluginControllerBoostrap); }
 			}
 			
-			require_once($controller_path); //Inclusion de ce fichier si il existe
-			return $controller_name;
+			require_once($controllerPath); //Inclusion de ce fichier si il existe
+			return $controllerName;
 			
 		} else { 
 
-			if(isset($sFolderPlugin)) { $message = "Le controller du plugin ".$this->request->controller." n'existe pas"." dans le fichier dispatcher ou n'est pas correctement installé"; }
+			if(isset($folderPlugin)) { $message = "Le controller du plugin ".$this->request->controller." n'existe pas"." dans le fichier dispatcher ou n'est pas correctement installé"; }
 			else { $message = "Le controller ".$this->request->controller." n'existe pas"." dans le fichier dispatcher"; }
 			$this->error($message);
 			die();	
 		}
 	}	
-	
+		
 /**
- * Cette fonction va charger un controller
- * @return $name Objet correspondant au type de controller souhaité
- */
-	public function loadController() {
-	
-		$file_name = $this->loadControllerFile();
-		$controller_name = Inflector::camelize($file_name); //On transforme le nom du fichier pour récupérer le nom du controller	
-		$controller =  new $controller_name($this->request); //Création d'une instance du controller souhaité dans lequel on injecte la request		
-		return $controller;
-	}	
-    
+ * Cette fonction va insérer dans le fichier de log les différentes erreurs rencontrées
+ * 
+ * @param varchar $message Message à insérer dans les logs
+ * @access	public
+ * @author	koéZionCMS
+ * @version 0.1 - 23/12/2011
+ */    
 	public function error($message) {
         
 		require_once(LIBS.DS.'config_magik.php');
