5bea9f06729ee89b08bd425234412bb4134b416a
koeZionCMS <github@koezion-cms.com>
2016-01-15 16:47:44 +0100

    Version du 15/01/2016 - 20160115.003
    Cf versions.xml

--- core/koeZion/system/constants.php
+++ core/koeZion/system/constants.php
@@ -16,109 +16,113 @@
  * @link        http://www.koezion-cms.com
  * @version		0.1
  */
-//$debut = microtime(true);
-
-
-
 ////////////////////////////////////////////////////////////////////////////////////
 //   MISE EN PLACE DES CONSTANTES VERS LES DIFFERENTS DOSSIERS (CHEMINS ASSOLUS)  //
 ////////////////////////////////////////////////////////////////////////////////////
 
+	///////////////////////////
+	//    DOSSIER WEBROOT    //
+	define('CKEDITOR', 			WEBROOT.DS.'ck'.DS.'ckeditor');
+	define('CKFINDER', 			WEBROOT.DS.'ck'.DS.'ckfinder');
+	define('CSS', 				WEBROOT.DS.'css');
+	define('FILES', 			WEBROOT.DS.'files');
+	define('IMG', 				WEBROOT.DS.'img');
+	define('JS', 				WEBROOT.DS.'js');
+	define('WEBROOT_PLUGINS', 	WEBROOT.DS.'plugins');
+	define('TEMPLATES', 		WEBROOT.DS.'templates');
+	define('UPLOAD', 			WEBROOT.DS.'upload');
 
-	define('CSS', WEBROOT.DS.'css'); 					//Chemin vers le dossier css
-	define('FILES', WEBROOT.DS.'files'); 				//Chemin vers le dossier files
-	define('IMG', WEBROOT.DS.'img'); 					//Chemin vers le dossier img
-	define('JS', WEBROOT.DS.'js'); 						//Chemin vers le dossier js
-	define('WEBROOT_PLUGINS', WEBROOT.DS.'plugins'); 	//Chemin vers le dossier plugins 
-	define('TEMPLATES', WEBROOT.DS.'templates'); 		//Chemin vers le dossier templates 
-	define('UPLOAD', WEBROOT.DS.'upload'); 				//Chemin vers le dossier upload
+	///////////////////////////
+	//    DOSSIER CONFIGS    //
+	define('CONFIGS_FILES', 	ROOT.DS.'configs'.DS.'files'); 		//Chemin vers le dossier des .ini
+	define('CONFIGS_HOOKS', 	ROOT.DS.'configs'.DS.'hooks'); 		//Chemin vers le dossier des hooks
+	define('CONFIGS_PLUGINS', 	ROOT.DS.'configs'.DS.'plugins'); 	//Chemin vers le dossier des plugins
 
-
-	define('CONFIGS_FILES', ROOT.DS.'configs'.DS.'files'); //Chemin vers le dossier des .ini
-	define('CONFIGS_HOOKS', ROOT.DS.'configs'.DS.'hooks'); //Chemin vers le dossier des hooks
-	define('CONFIGS_PLUGINS', ROOT.DS.'configs'.DS.'plugins'); //Chemin vers le dossier des plugins
-
+	////////////////////////
+	//    DOSSIER CORE    //	
+	define('CORE', 				ROOT.DS.'core'); 		//Chemin vers le coeur de l'application
+		define('CAKEPHP', 		CORE.DS.'CakePhp'); 	//Chemin vers les librairies de cakePhp
+		define('GETTEXT', 		CORE.DS.'GetText'); 	//Chemin vers les librairies de getText
+		define('LIBS', 			CORE.DS.'Libs'); 		//Chemin vers les librairies diverses
+		define('PEAR', 			CORE.DS.'Pear'); 		//Chemin vers les librairies de Pear
+		define('SWIFTMAILER', 	CORE.DS.'SwiftMailer'); //Chemin vers les librairies de SwiftMailer
 	
-
-
-define('CORE', ROOT.DS.'core'); //Chemin vers le coeur de l'application
-	define('CAKEPHP', CORE.DS.'CakePhp'); //Chemin vers les librairies de cakePhp
-	define('GETTEXT', CORE.DS.'GetText'); //Chemin vers les librairies de getText
-	define('LIBS', CORE.DS.'Libs'); //Chemin vers les librairies diverses
-	define('PEAR', CORE.DS.'Pear'); //Chemin vers les librairies de Pear
-	define('SWIFTMAILER', CORE.DS.'SwiftMailer'); //Chemin vers les librairies de SwiftMailer
+	/////////////////////////////////////////////////	
+	//    CHEMIN VERS LES LIBRAIRIES DE KOEZION    //
+	define('KOEZION', 					CORE.DS.'koeZion'); 			//Chemin vers les librairies de Koezion	
+		define('CONFIGS', 				KOEZION.DS.'configs'); 			//Chemin vers le dossier configs
+			define('CONFIGS_VERSIONS', 	CONFIGS.DS.'versions'); 		//Chemin vers le dossier des fichiers de versions			
+		define('CONTROLLERS', 			KOEZION.DS.'controllers'); 		//Chemin vers le dossier des contrôleurs
+			define('COMPONENTS', 		CONTROLLERS.DS.'components'); 	//Chemin vers le dossier des composants	
+		define('MODELS', 				KOEZION.DS.'models'); 			//Chemin vers le dossier des modèles
+			define('BEHAVIORS', 		MODELS.DS.'behaviors'); 		//Chemin vers le dossier des comportements			
+		define('SYSTEM', 				KOEZION.DS.'system'); 			//Chemin vers le dossier des librairies système		
+		define('VIEWS', 				KOEZION.DS.'views'); 			//Chemin vers le dossier des vues
+			define('ELEMENTS', 			VIEWS.DS.'elements'); 			//Chemin vers le dossier des élements
+			define('HELPERS', 			VIEWS.DS.'helpers'); 			//Chemin vers le dossier des helpers
+			define('LAYOUT', 			VIEWS.DS.'layout'); 			//Chemin vers le dossier des layouts
 	
-/////////////////////////////////////////////////	
-//    CHEMIN VERS LES LIBRAIRIES DE KOEZION    //
-/////////////////////////////////////////////////	
-define('KOEZION', CORE.DS.'koeZion'); //Chemin vers les librairies de Koezion
+	////////////////////////////////////////////////////////
+	//    CHEMIN VERS LE DOSSIER D'INSTALLATION DU CMS    //
+	//Remarque : une fois l'installation terminée il faut supprimer le dossier install
+	define('INSTALL', 					ROOT.DS.'install'); 		//Chemin vers le dossier d'installation
+		define('INSTALL_FILES', 		INSTALL.DS.'files'); 		//Chemin vers le dossier des fichiers copiés lors du processus d'installation
+		define('INSTALL_FUNCTIONS', 	INSTALL.DS.'functions'); 	//Chemin vers les fichiers contenants les fonctions nécessaires au processus d'installation
+		define('INSTALL_INCLUDE', 		INSTALL.DS.'include'); 		//Chemin vers les fichiers inclus lors du processus d'installation
+		define('INSTALL_VALIDATE', 		INSTALL.DS.'validate'); 	//Chemin vers les fichiers de validation utilisés lors du processus d'installation
 	
-	define('CONFIGS', KOEZION.DS.'configs'); //Chemin vers le dossier config
-	define('CONFIGS_VERSIONS', CONFIGS.DS.'versions'); //Chemin vers le dossier des fichiers de versions
+	///////////////////////////////////////////////////////////////////
+	//    CHEMIN VERS LE DOSSIER CONTENANT LES DIFFERENTS PLUGINS    //
+	define('PLUGINS', ROOT.DS.'plugins');	
 		
-	define('CONTROLLERS', KOEZION.DS.'controllers'); //Chemin vers le dossier des contrôleurs
-	define('COMPONENTS', CONTROLLERS.DS.'components'); //Chemin vers le dossier des composants
+	/////////////////////////////////////////////////////////////////////
+	//    CHEMIN VERS LE DOSSIER CONTENANT LES FICHIERS TEMPORAIRES    //
+	define('TMP', 			ROOT.DS.'tmp'); 	//Chemin vers le dossier temporaire
+		define('CACHE', 	TMP.DS.'cache'); 	//Chemin vers le dossier contenant les fichiers de cache
+		define('LOGS', 		TMP.DS.'logs'); 	//Chemin vers les fichiers de logs
 
-	define('MODELS', KOEZION.DS.'models'); //Chemin vers le dossier des modèles
-	define('BEHAVIORS', MODELS.DS.'behaviors'); //Chemin vers le dossier des comportements
-		
-	define('SYSTEM', KOEZION.DS.'system'); //Chemin vers le dossier des vues
-	
-	define('VIEWS', KOEZION.DS.'views'); //Chemin vers le dossier des vues
-	define('ELEMENTS', VIEWS.DS.'elements'); //Chemin vers le dossier des élements
-	define('HELPERS', VIEWS.DS.'helpers'); //Chemin vers le dossier des helpers
-	define('LAYOUT', VIEWS.DS.'layout'); //Chemin vers le dossier des layouts
-/////////////////////////////////////////////////
-	
-
-define('INSTALL', ROOT.DS.'install'); //Chemin vers le dossier des traductions
-	define('INSTALL_FILES', INSTALL.DS.'files'); //Chemin vers le dossier des traductions
-	define('INSTALL_FUNCTIONS', INSTALL.DS.'functions'); 	//Chemin vers les fichiers contenants les fonctions
-	define('INSTALL_INCLUDE', INSTALL.DS.'include'); 		//Chemin vers les fichiers include de configuration
-	define('INSTALL_VALIDATE', INSTALL.DS.'validate'); 	//Chemin vers les fichiers de validation
-	
-define('PLUGINS', ROOT.DS.'plugins'); //Chemin vers le dossier des plugins	
-	
-define('TMP', ROOT.DS.'tmp'); //Chemin vers le dossier temporaire
-	define('CACHE', TMP.DS.'cache'); //Chemin vers le dossier cache
-	define('LOGS', TMP.DS.'logs'); //Chemin vers le dossier logs
-
-define('VENDORS', ROOT.DS.'vendors'); //Chemin vers le dossier vendors
-
-
-////////////////////////////////////////////////////////////////////////////////////	
+	///////////////////////////////////////////////////////////////////
+	//    CHEMIN VERS LE DOSSIER CONTENANT LES LIBRAIRIES ANNEXES    //
+	define('VENDORS', ROOT.DS.'vendors');
 	
 /////////////////////////////////////////////////////////
 //   MISE EN PLACE DU CHEMIN RELATIF VERS LE WEBROOT   //
-//http://www.siteduzero.com/forum-83-692076-p1-base-url.html
-//define('BASE_URL', dirname(dirname($_SERVER['SCRIPT_NAME']))); //Chemin relatif vers le coeur de l'application --> OLD
-//Ne marche plus si le site est dans plus d'un sous dossier
-//24/07/2012 --> Correctif : maintenant tout fonctionne même si plus d'un sous dossier, correction de la génération de la variable $baseUrl
-$baseUrl = '';
-$scriptPath = preg_split("#[\\\\/]#", dirname(__FILE__), -1, PREG_SPLIT_NO_EMPTY);
-$urlPath = preg_split("#[\\\\/]#", $_SERVER['REQUEST_URI'], -1, PREG_SPLIT_NO_EMPTY);
-
-foreach($urlPath as $k => $v) {
-	
-	$key = array_search($v, $scriptPath);
-	if($key !== false) $baseUrl .= "/".$v;
-}
-define('BASE_URL', $baseUrl); //Chemin relatif vers le coeur de l'application
 /////////////////////////////////////////////////////////
 
-define('GENERATOR_META', 'koéZion CMS - CMS OPENSOURCE PHP');
-define('GENERATOR_LINK', '<p id="powered_by" style="position:absolute;width:20px;bottom:5px;right:5px;font-size:8px;margin-bottom:0;height:20px;opacity:.3;z-index:10000"><a href="http://www.koezion-cms.com" title="Propulsé par koéZionCMS - CMS opensource PHP" style="width:20px;height:20px;text-indent:-9999px;display:block;background: url('.BASE_URL.'/img/logo_koezion_mini.png) no-repeat top right transparent;padding: 0;margin: 0;border: none;">Propulsé par koéZionCMS - CMS opensource PHP</a></p>');
+	//http://www.siteduzero.com/forum-83-692076-p1-base-url.html
+	//define('BASE_URL', dirname(dirname($_SERVER['SCRIPT_NAME']))); //Chemin relatif vers le coeur de l'application --> OLD
+	//Ne marche plus si le site est dans plus d'un sous dossier
+	//24/07/2012 --> Correctif : maintenant tout fonctionne même si plus d'un sous dossier, correction de la génération de la variable $baseUrl
+	$baseUrl = '';
+	$scriptPath = preg_split("#[\\\\/]#", dirname(__FILE__), -1, PREG_SPLIT_NO_EMPTY);
+	$urlPath = preg_split("#[\\\\/]#", $_SERVER['REQUEST_URI'], -1, PREG_SPLIT_NO_EMPTY);
+	
+	foreach($urlPath as $k => $v) {
+		
+		$key = array_search($v, $scriptPath);
+		if($key !== false) $baseUrl .= "/".$v;
+	}
+	define('BASE_URL', $baseUrl); //Chemin relatif vers le coeur de l'application
+	
+////////////////////////////////////////////////////////////////
+//    DEFINITION DES DEUX CONSTANTES DE DECLARATION DU CMS    //
+////////////////////////////////////////////////////////////////
 
-////////////////////////////////////////////////////
-//    RECUPERATION DES CONFIGURATIONS DU COEUR    //
-require_once(LIBS.DS.'config_magik.php');
-$cfg = new ConfigMagik(CONFIGS_FILES.DS.'core.ini', true, false);
-$coreConfs = $cfg->keys_values();
+	define('GENERATOR_META', 'koéZion CMS - CMS OPENSOURCE PHP');
+	define('GENERATOR_LINK', '<p id="powered_by" style="position:absolute;width:20px;bottom:5px;right:5px;font-size:8px;margin-bottom:0;height:20px;opacity:.3;z-index:10000"><a href="http://www.koezion-cms.com" title="Propulsé par koéZionCMS - CMS opensource PHP" style="width:20px;height:20px;text-indent:-9999px;display:block;background: url('.BASE_URL.'/img/logo_koezion_mini.png) no-repeat top right transparent;padding: 0;margin: 0;border: none;">Propulsé par koéZionCMS - CMS opensource PHP</a></p>');
 
-foreach($coreConfs as $coreConfField => $coreConfValue) {
+//////////////////////////////////////////////////////////////////////////////////////
+//    RECUPERATION DES CONFIGURATIONS DU COEUR POUR TRANSFORMATION EN CONSTANTES    //
+//////////////////////////////////////////////////////////////////////////////////////
 
-	$coreConfField = strtoupper($coreConfField);
-	if(!defined($coreConfField)) { define($coreConfField, $coreConfValue); }
-}
-
-if(!defined('BACKOFFICE_TEMPLATE')) { define('BACKOFFICE_TEMPLATE', 'bo_godzilla'); }
+	require_once(LIBS.DS.'config_magik.php');
+	$cfg = new ConfigMagik(CONFIGS_FILES.DS.'core.ini', true, false);
+	$coreConfs = $cfg->keys_values();
+	
+	foreach($coreConfs as $coreConfField => $coreConfValue) {
+	
+		$coreConfField = strtoupper($coreConfField);
+		if(!defined($coreConfField)) { define($coreConfField, $coreConfValue); }
+	}
+	
+	if(!defined('BACKOFFICE_TEMPLATE')) { define('BACKOFFICE_TEMPLATE', 'bo_godzilla'); }
--- webroot/index.php
+++ webroot/index.php
@@ -17,6 +17,7 @@
  * @link        http://www.koezion-cms.com
  * @version		0.1
  */
+//$debut = microtime(true);
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //    DEFINITION DU CHEMIN ET CHARGEMENT DE LA LIBRAIRIE CONTENANT L'ENSEMBLE DES CONSTANTES DU SYSTEME    //
@@ -39,39 +40,8 @@ if(!file_exists(CONFIGS_FILES.DS.'database.ini')) {
 }
 
 require_once SYSTEM.DS.'bootstrap.php'; //Premier fichier lancé par l'application
-
-//21/08/2015 by FI - Rajout du filtrage des crawlers 
-/////////////////////////////////
-//    FILTRAGE DES CRAWLERS    //
-/////////////////////////////////
-if(defined('FILTERING_CRAWLERS') && FILTERING_CRAWLERS) {
-	
-	$referer 	= strtolower(isset($_SERVER['HTTP_REFERER']) ? $_SERVER['HTTP_REFERER'] : ''); //Récupération du referer
-	$ip 		= $_SERVER["REMOTE_ADDR"]; //Récupération de l'ip
-	
-	require_once(MODELS.DS.'unwanted_crawler.php'); //Chargement de la librairie
-	$UwantedCrawlerModel 	= new UnwantedCrawler(); //Création d'un objet
-	$uwantedCrawlers 		= $UwantedCrawlerModel->find(array('conditions' => array('online' => 1))); //Récupération des données en base
-	
-	//On parcours la liste des crawlers
-	foreach($uwantedCrawlers as $uwantedCrawler) {
-		
-		//$refererCheck = stristr($referer, $uwantedCrawler['url']) != FALSE; //Contrôle sur le referer
-		//$ipCheck 		= stristr($ip, $uwantedCrawler['ip']) != FALSE; //Contrôle sur l'adresse IP
-		
-		$uwantedCrawlerUrl 	= strtolower($uwantedCrawler['url']);
-		$refererCheck 		= substr_count($referer, $uwantedCrawlerUrl); //Contrôle sur le referer
-		$ipCheck 			= substr_count($ip, $uwantedCrawler['ip']); //Contrôle sur l'adresse IP
-
-		//Si l'un ou l'autre des tests est vrai alors on affiche la page d'erreur
-		if($refererCheck || $ipCheck) {
-	    	
-			header("HTTP/1.0 404 Not Found"); //On lance une erreur 404
-	        readfile(SYSTEM.DS.'crawlers404.php'); //On charge le fichier qui affiche l'erreur
-	        die; //On ne permet pas l'exécution du code situé après cette instruction
-		}
-	}
-}
+require_once SYSTEM.DS.'crawlers.php'; //Gestion des crawlers
 
 new Dispatcher(); //On créé une instance de Dispatcher
+
 //if(Configure::read('debug') > 0) { Configure::write('timerExec', round(microtime(true) - $debut, 5)); }
