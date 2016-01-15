4169f2f537adb763b07e91bcb22ef03878a4b5ca
koeZionCMS <github@koezion-cms.com>
2016-01-15 18:18:35 +0100

    Version du 15/01/2016 - 20160115.006
    Cf versions.xml

--- webroot/constants.php
+++ webroot/constants.php
@@ -28,7 +28,7 @@
 	//Utile pour modifier le chemin vers le dossier core par exemple...
 	//Par défaut le conseil du jour est de copier EXACTEMENT le code qui se trouve entre les {} du else et de ne modifier que ce dont vous avez besoin.
 	//Ne surtout PAS SUPPRIMER de constante sous peine de voir votre application planter
-	if(file_exists(ROOT.DS.'tmp'.DS.'constants.php')) { require_once ROOT.DS.'tmp'.DS.'constants.php'; }
+	if(file_exists(ROOT.DS.'tmp'.DS.'constants_rewrite.php')) { require_once ROOT.DS.'tmp'.DS.'constants_rewrite.php'; }
 	else {
 
 		///////////////////////////
