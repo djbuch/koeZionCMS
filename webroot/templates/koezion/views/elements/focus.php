f4c2fd946a43eeed6a186ce064ebcecc17e11197
koeZionCMS <github@koezion-cms.com>
2016-05-27 10:35:35 +0200

    Version du 27/05/2016 - 20160527.003
    Cf versions.xml

--- webroot/templates/koezion/views/elements/focus.php
+++ webroot/templates/koezion/views/elements/focus.php
@@ -4,16 +4,21 @@
 			<div class="row">			
 				<?php 
 				$cpt = 1;
-				foreach($focus as $v) { 
-				
+				if(count($focus) == 3) { $focusCss = 'col-lg-4'; $cptLimit = 3; }
+				else if(count($focus) == 4) { $focusCss = 'col-lg-3'; $cptLimit = 4; }
+				foreach($focus as $v) {
 					?>
-					<div class="item col-lg-3">
-						<img class="img-rounded" src="<?php echo $v['details_img']; ?>" alt="<?php echo $v['details_title']; ?>" />
-						<h2><?php echo $v['details_title']; ?></h2>
-						<div class="focus_content"><?php echo $v['details_content']; ?></div>						
+					<div class="item <?php echo $focusCss; ?>">
+						<?php if(!empty($v['details_img'])) { ?><img class="img-rounded" src="<?php echo $v['details_img']; ?>" alt="<?php echo $v['details_title']; ?>" /><?php } ?>
+						<?php if(!empty($v['details_title'])) { ?><h2><?php echo $v['details_title']; ?></h2><?php } ?>
+						<?php if(!empty($v['details_content'])) { ?><div class="focus_content"><?php echo $v['details_content']; ?></div><?php } ?>
 					</div>
 					<?php
-					if($cpt == 4) { ?></div><div class="row mb30"><?php }
+					if($cpt == $cptLimit) { 
+						
+						?></div><div class="row mb30"><?php
+						$cpt = 0; 
+					}
 					$cpt++;
 				} 
 				?>
